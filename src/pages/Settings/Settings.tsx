4995b473948ad20e45d238839785f8da8aafa10b
Albert Lu <lualbert88@gmail.com>
2022-08-08 18:48:35 +0800

    Remove snackbar component
    Socketio connection problem couldn't be resolved yet

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,3 +1,16 @@
+import { useAppSelector } from "@/app/hooks";
+import { RootState } from "@/app/store";
+
 export default function Settings() {
-  return <div className="w-full">Settings</div>;
+  const UserData = useAppSelector((state: RootState) => state.user.data);
+
+  return (
+    <div className="w-full">
+      <div>{`Name: ${UserData?.first_name} ${UserData?.last_name}`}</div>
+      <hr className="sidebar-hr" />
+      <div>{`Phone: ${UserData?.phone}`}</div>
+      <hr className="sidebar-hr" />
+      <div>{`Username: ${UserData?.username}`}</div>
+    </div>
+  );
 }
