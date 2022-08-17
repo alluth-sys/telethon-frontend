38b4814dfc97aee2e4692ce679d66d8324cd2fb4
Albert Lu <lualbert88@gmail.com>
2022-08-17 13:54:02 +0800

    Added check connection backdrop

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -19,27 +19,6 @@ export default function PrivateRoute({ isLogin, data }: IUser) {
     return <Navigate to="/signin" replace />;
   }
 
-  //const socket = React.useContext(SocketContext);
-  // const [isSocketConnected, setSocketConnected] = React.useState(
-  //   socket.connected
-  // );
-
-  // const MINUTE_MS = 10000;
-  // React.useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     socket.emit("ping");
-  //     socket.on("pong", (res) => {
-  //       console.log(res);
-  //       setSocketConnected(true);
-  //     });
-  //   }, MINUTE_MS);
-
-  //   return () => {
-  //     clearInterval(interval);
-  //     socket.off("pong");
-  //   };
-  // }, []);
-
   return (
     <div className="flex">
       <SideBar />
