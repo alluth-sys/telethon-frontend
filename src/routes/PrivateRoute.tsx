eb224c7af3a3739a8bd3299444830243134dac4d
Albert Lu <lualbert88@gmail.com>
2022-08-17 09:57:26 +0800

    Added Check Connection SnackBar

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 // Router
 import { Navigate, Outlet } from "react-router-dom";
 
@@ -7,15 +8,43 @@ import SideBar from "@/components/SideBar";
 // Types
 import { IUser } from "@/states/user/userSlice";
 
+// Socket
+import { SocketContext } from "@/service/Socket";
+
+// SnackBar
+import ConnectionSnackBar from "@/components/ConnectionSnackBar";
+
 export default function PrivateRoute({ isLogin, data }: IUser) {
   if (!isLogin && data === null) {
     return <Navigate to="/signin" replace />;
   }
 
+  //const socket = React.useContext(SocketContext);
+  // const [isSocketConnected, setSocketConnected] = React.useState(
+  //   socket.connected
+  // );
+
+  // const MINUTE_MS = 10000;
+  // React.useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     socket.emit("ping");
+  //     socket.on("pong", (res) => {
+  //       console.log(res);
+  //       setSocketConnected(true);
+  //     });
+  //   }, MINUTE_MS);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //     socket.off("pong");
+  //   };
+  // }, []);
+
   return (
     <div className="flex">
       <SideBar />
       <Outlet />
+      <ConnectionSnackBar />
     </div>
   );
 }
