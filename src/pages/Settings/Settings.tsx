5f8585bdb6bbcb0c44cb4371d357a518bc219b70
Albert Lu <lualbert88@gmail.com>
2022-09-15 00:25:55 +0800

    Added Advance Settings Component

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { useAppSelector } from "@/app/hooks";
 import { RootState } from "@/app/store";
 
-import { Typography, Button } from "@mui/material";
-
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import Link from "@mui/material/Link";
+import {
+  Typography,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Link,
+} from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
-import SaveIcon from "@mui/icons-material/Save";
 
+import ProfilePicture from "@/components/MessageBox/ProfilePicture";
+import AdvanceSettings from "@/components/AdvanceSettings";
 import Spacer from "react-spacer";
+
+// Translation
 import { useTranslation } from "react-i18next";
 
 export default function Settings() {
@@ -25,6 +29,7 @@ export default function Settings() {
     setLanguage(event.target.value);
   };
 
+  // Detect Language Change
   React.useEffect(() => {
     if (language === "English") {
       i18n.changeLanguage("en");
@@ -34,85 +39,91 @@ export default function Settings() {
   }, [language]);
 
   return (
-    <div className="w-full">
-      <div className="m-8 flex w-fit p-8 rounded-xl hover:shadow-xl transition-all duration-200 ease-linear">
-        <div className="m-4">
-          <img
-            src={`data:image/jpeg;base64,${UserData?.profile_pic}`}
-            className="w-36 h-36 rounded-full"
-          />
-        </div>
-        <div className="flex justify-center items-center ">
-          <div>
-            <Typography variant="h4" style={{ fontWeight: 400 }}>
-              {UserData?.last_name}
-              {UserData?.first_name}
-            </Typography>
-            <Typography style={{ color: "grey" }}>
-              {UserData?.username}
-            </Typography>
-            <Typography
-              style={{ color: "grey" }}
-            >{`+${UserData?.phone}`}</Typography>
+    <div className="w-full flex">
+      {/* Left */}
+      <div className="m-8 w-2/4">
+        <div className="m-8 flex w-fit p-8 rounded-xl shadow-xl">
+          {/* User Image */}
+          <div className="m-4">
+            <ProfilePicture
+              uid={UserData?.first_name}
+              imgSrc={`data:image/jpeg;base64,${UserData?.profile_pic}`}
+            />
+          </div>
+          {/* User Data */}
+          <div className="flex justify-center items-center ">
+            <div>
+              <Typography variant="h4" style={{ fontWeight: 400 }}>
+                {UserData?.last_name} {UserData?.first_name}
+              </Typography>
+              <Typography style={{ color: "grey" }}>
+                {UserData?.username}
+              </Typography>
+              <Typography
+                style={{ color: "grey" }}
+              >{`+${UserData?.phone}`}</Typography>
+            </div>
+          </div>
+          {/* Edit Button */}
+          <div className="flex justify-end items-end pl-12">
+            <EditIcon style={{ color: "#3b82f6" }} />
           </div>
         </div>
-        <div className="flex justify-end items-end pl-12">
-          <EditIcon style={{ color: "#3b82f6" }} />
+        {/* Language & Font */}
+        <div className="m-8 flex">
+          <FormControl>
+            <FormLabel id="language-buttons-group-label">
+              {t("Language")}
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="language-buttons-group-label"
+              defaultValue="English"
+              name="radio-buttons-group"
+              onChange={setLanguageHandler}
+            >
+              <FormControlLabel
+                value="English"
+                control={<Radio />}
+                label={t("English")}
+              />
+              <FormControlLabel
+                value="Chinese"
+                control={<Radio />}
+                label={t("Chinese")}
+              />
+            </RadioGroup>
+          </FormControl>
+          <Spacer width={"10%"} />
+          <FormControl>
+            <FormLabel id="fontsize-buttons-group-label">
+              {t("FontSize")}
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="fontsize-buttons-group-label"
+              defaultValue="1"
+              name="radio-buttons-group"
+              onChange={(event) => {
+                setLanguage(event.target.value);
+              }}
+            >
+              <FormControlLabel value="1" control={<Radio />} label="1" />
+              <FormControlLabel value="1.5" control={<Radio />} label="1.5" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+        {/* LINKS */}
+        <div className="m-8">
+          <Link href="#">
+            <Typography style={{ color: "grey" }}>{t("FAQ")}</Typography>
+          </Link>
+          <Link href="#">
+            <Typography style={{ color: "grey" }}>{t("DevInfo")}</Typography>
+          </Link>
         </div>
       </div>
-      <div className="m-8 flex">
-        <FormControl>
-          <FormLabel id="language-buttons-group-label">
-            {t("Language")}
-          </FormLabel>
-          <RadioGroup
-            aria-labelledby="language-buttons-group-label"
-            defaultValue="English"
-            name="radio-buttons-group"
-            onChange={setLanguageHandler}
-          >
-            <FormControlLabel
-              value="English"
-              control={<Radio />}
-              label={t("English")}
-            />
-            <FormControlLabel
-              value="Chinese"
-              control={<Radio />}
-              label={t("Chinese")}
-            />
-          </RadioGroup>
-        </FormControl>
-        <Spacer width={"10%"} />
-        <FormControl>
-          <FormLabel id="fontsize-buttons-group-label">
-            {t("FontSize")}
-          </FormLabel>
-          <RadioGroup
-            aria-labelledby="fontsize-buttons-group-label"
-            defaultValue="1"
-            name="radio-buttons-group"
-            onChange={(event) => {
-              setLanguage(event.target.value);
-            }}
-          >
-            <FormControlLabel value="1" control={<Radio />} label="1" />
-            <FormControlLabel value="1.5" control={<Radio />} label="1.5" />
-          </RadioGroup>
-        </FormControl>
-      </div>
-      <div className="m-8">
-        <Link href="#">
-          <Typography style={{ color: "grey" }}>{t("FAQ")}</Typography>
-        </Link>
-        <Link href="#">
-          <Typography style={{ color: "grey" }}>{t("DevInfo")}</Typography>
-        </Link>
-      </div>
-      <div className="flex m-8">
-        <Button>
-          <SaveIcon fontSize="large" />
-        </Button>
+      {/* Right */}
+      <div className="m-8 w-2/4">
+        <AdvanceSettings />
       </div>
     </div>
   );
