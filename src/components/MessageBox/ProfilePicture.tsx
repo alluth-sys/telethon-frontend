3941a17af91886c8f7a04ab003d54dd360827c58
Albert Lu <lualbert88@gmail.com>
2022-09-15 00:24:50 +0800

    Created Profile Picture Component

---
+++ src/components/MessageBox/ProfilePicture.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    width: "10%",
+    height: "10%",
+    borderRadius: "50%",
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+type TProps = { uid: string | undefined; imgSrc: string };
+
+export default function ProfilePicture({ uid, imgSrc }: TProps) {
+  return (
+    <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      variant="dot"
+    >
+      <Avatar alt={uid} src={imgSrc} sx={{ width: 144, height: 144 }} />
+    </StyledBadge>
+  );
+}
