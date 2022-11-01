243cee20c7ec5173f7930d11541dcbb6d0f0491d
c1l1mo600 <c1l1mo600@gmail.com>
2022-11-01 13:16:45 +0800

    patch : fix typo

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -53,12 +53,12 @@ export default function chat() {
         </Collapse>
       </div>
       <div className="grow grid content-start bg-[url('./resources/bg5.png')]">
-        <TopArea
+        {<TopArea
           collapsed={collapsed}
           setCollapse={setCollapse}
           focus={focus}
         />
-        ||<div style={{ height: "8vh" }}></div>
+        ||<div style={{ height: "8vh" }}></div>}
         <div
           style={{
             height: "1px",
